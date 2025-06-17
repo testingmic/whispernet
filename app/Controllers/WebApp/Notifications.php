9066fa6fd7c7af3de6c79a0ff31ff467fc452bf7
Emmanuel Obeng <emmallob14@gmail.com>
2025-06-17 19:51:26 +0000

    Update the files

--- app/Controllers/WebApp/Notifications.php
+++ app/Controllers/WebApp/Notifications.php
@@ -6,8 +6,27 @@ use App\Controllers\WebAppController;
 
 class Notifications extends WebAppController
 {
-    public function index()
-    {
-        return $this->templateObject->loadPage('notifications', ['pageTitle' => 'Notifications']);
+    
+    public function index() {
+
+        $notifications = [];
+
+        $types = ['like', 'comment', 'follow', 'message'];
+        
+        for($i = 0; $i < 10; $i++) {
+            $notifications[] = [
+                'id' => $i,
+                'type' => $types[array_rand($types)],
+                'message' => 'John Doe liked your post',
+                'time' => date('H:i:s', strtotime('-' . $i . ' minutes')),
+                'read' => false,
+                'time_ago' => '30min ago'
+            ];
+        }
+
+        return $this->templateObject->loadPage('notifications', [
+            'pageTitle' => 'Notifications',
+            'notifications' => $notifications
+        ]);
     }
 }
