1925ac4254f2018c452043b428730e8dfe4166da
Emmanuel Obeng <emmallob14@gmail.com>
2025-06-18 19:46:12 +0000

    Remove the hidden state

--- app/Views/templates/footer.php
+++ app/Views/templates/footer.php
@@ -28,7 +28,7 @@
           <span class="text-xs mt-1">Profile</span>
         </a>
       <?php } ?>
-      <button id="installButton" class="hidden flex flex-col items-center justify-center px-3 py-2 text-gray-600 hover:text-blue-500">
+      <button id="installButton" class="flex flex-col items-center justify-center px-3 py-2 text-gray-600 hover:text-blue-500">
         <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4" />
         </svg>
