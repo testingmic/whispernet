037a59ea58816104dbae0be090a3c6e84bc49431
Emmanuel Obeng <emmallob14@gmail.com>
2025-06-19 21:31:14 +0000

    Begin the chat integration

--- app/Views/feed.php
+++ app/Views/feed.php
@@ -144,7 +144,7 @@
                     <svg class="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4v16m8-8H4"></path>
                     </svg>
-                    Create First Post
+                    Create A Post
                 </button>
             </div>
         </div>
