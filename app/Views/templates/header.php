4b60b9b1de413d3649a4b7d042f464d336e29844
Emmanuel Obeng <emmallob14@gmail.com>
2025-06-19 07:41:20 +0000

    Update text

--- app/Views/templates/header.php
+++ app/Views/templates/header.php
@@ -159,7 +159,7 @@ $favicon_color = $favicon_color ?? 'dashboard';
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"></path>
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"></path>
                   </svg>
-                  <span>Current location will be used.</span>
+                  <span>Your current location.</span>
                 </div>
                 <button
                   type="submit"
