638e739646cf8521714fb76e9141234caf34a89d
Emmanuel Obeng <emmallob14@gmail.com>
2025-06-19 06:32:15 +0000

    Processing of new post notifications and counter rendering

--- app/Controllers/Templates.php
+++ app/Controllers/Templates.php
@@ -48,7 +48,7 @@ class Templates extends BaseController
 
         return [
             'baseUrl' => $urlPath,
-            'version' => '1.1.10',
+            'version' => '1.1.11',
             'websocketUrl' => $socketUrl,
             'userData' => session()->get('userData'),
             'userLoggedIn' => $this->user_loggedin(),
