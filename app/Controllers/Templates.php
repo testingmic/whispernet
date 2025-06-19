f5b3052db5e35c9d9448ee28fd0963ec8fe7f916
Emmanuel Obeng <emmallob14@gmail.com>
2025-06-19 15:41:13 +0000

    i need to know the error

--- app/Controllers/Templates.php
+++ app/Controllers/Templates.php
@@ -48,7 +48,7 @@ class Templates extends BaseController
 
         return [
             'baseUrl' => $urlPath,
-            'version' => '1.1.201',
+            'version' => '1.1.2044',
             'websocketUrl' => $socketUrl,
             'userData' => session()->get('userData'),
             'userLoggedIn' => $this->user_loggedin(),
