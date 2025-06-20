4c9d3f3d0472d590ec57275b7331a065b420fbe4
Emmanuel Obeng <emmallob14@gmail.com>
2025-06-20 18:48:45 +0000

    udate

--- app/Controllers/Templates.php
+++ app/Controllers/Templates.php
@@ -55,7 +55,7 @@ class Templates extends BaseController
 
         return [
             'baseUrl' => $urlPath,
-            'version' => '1.2.34',
+            'version' => '1.2.35',
             'userId' => (int) $this->sessionObject->user_id,
             'websocketUrl' => $socketUrl,
             'userData' => $this->sessionObject->get('userData'),
