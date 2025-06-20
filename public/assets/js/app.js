25eb287e2c69a05dfe0cfa15c86bb7354f9eb9ba
Emmanuel Obeng <emmallob14@gmail.com>
2025-06-20 18:01:57 +0000

    Display position

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -656,6 +656,7 @@ const PostManager = {
             if(data.data.length == 0) {
                 document.getElementById('oldPostsContainer').classList.add('hidden');
             }
+            $(`.position-display`).html(`${longitude},${latitude}`);
             if(this.userLocation && $(`.location-display`).length > 0) {
                 $(`.location-display`).html(`${this.userLocation.city}, ${this.userLocation.country}`);
             }
