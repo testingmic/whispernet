6cd5b22a66f2595dce31014c3c812b353db79ba9
Emmanuel Obeng <emmallob14@gmail.com>
2025-06-20 17:37:04 +0000

    Deal with the notifications error

---
+++ app/Controllers/Notifications/Notifications.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace App\Controllers\Notifications;
+
+use App\Controllers\LoadController;
+use App\Libraries\Routing;
+
+class Notifications extends LoadController {
+
+    public function recent() {
+        
+        return Routing::success(['data' => 'Notifications fetched successfully']);
+        
+    }
+    
+}
