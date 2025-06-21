0af4431cb1cf395cf26a60e58d78f4cc354f9bd7
Emmanuel Obeng <emmallob14@gmail.com>
2025-06-21 18:26:03 +0000

    Remove install

--- app/Views/setup/signup.php
+++ app/Views/setup/signup.php
@@ -98,33 +98,5 @@
                 </button>
             </div>
         </form>
-
-        <!-- Social Signup -->
-        <div class="mt-6 ">
-            <div class="relative">
-                <div class="absolute inset-0 flex items-center">
-                    <div class="w-full border-t border-gray-300 dark:border-gray-600"></div>
-                </div>
-                <div class="relative flex justify-center text-sm">
-                    <span class="px-2 bg-gray-50 dark:bg-gray-900 text-gray-500 dark:text-gray-400"></span>
-                </div>
-            </div>
-
-            <div class="mt-6 grid grid-cols-1">
-                <a href="<?= $baseUrl; ?>/dashboard/install" type="button"
-                    class="w-full inline-flex justify-center py-2 px-4 border border-gray-300 dark:border-gray-600 rounded-md shadow-sm bg-white dark:bg-gray-800 text-sm font-medium text-gray-500 dark:text-gray-400 hover:bg-gray-50 dark:hover:bg-gray-700">
-                    <svg xmlns="http://www.w3.org/2000/svg"
-                        class="w-6 h-6 text-gray-700"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                        stroke-width="2">
-                        <path stroke-linecap="round" stroke-linejoin="round"
-                            d="M4 16v2a2 2 0 002 2h12a2 2 0 002-2v-2M7 10l5 5m0 0l5-5m-5 5V4" />
-                    </svg>
-                    Click here to install
-                </a>
-            </div>
-        </div>
     </div>
 </div>
