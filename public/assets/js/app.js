da32bacca5dd09e3a91ae611bcc4dbbe1221f5d8
Emmanuel Obeng <emmallob14@gmail.com>
2025-06-22 16:15:17 +0000

    Add onclick listener

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -964,7 +964,7 @@ const PostManager = {
             }
             <div class="flex items-center justify-between">
                 <div class="flex items-center space-x-4">
-                    <button class="flex items-center space-x-1 text-gray-500 hover:text-blue-500 dark:text-gray-800 dark:hover:text-blue-400 transition-colors" onclick="event.stopPropagation();" data-posts-id="${post.post_id}">
+                    <button ${!single ? `onclick="return PostManager.changeDirection(${post.post_id})"` : ''} class="flex items-center space-x-1 text-gray-500 hover:text-blue-500 dark:text-gray-800 dark:hover:text-blue-400 transition-colors" onclick="event.stopPropagation();" data-posts-id="${post.post_id}">
                         <svg class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z" />
                         </svg>
@@ -982,7 +982,7 @@ const PostManager = {
                         </svg>
                         <span data-posts-id-downvotes="${post.post_id}">${post.downvotes}</span>
                     </button>
-                    <button class="flex items-center space-x-1 text-gray-500 dark:text-gray-800" onclick="event.stopPropagation();" data-posts-id="${post.post_id}">
+                    <button ${!single ? `onclick="return PostManager.changeDirection(${post.post_id})"` : ''} class="flex items-center space-x-1 text-gray-500 dark:text-gray-800" onclick="event.stopPropagation();" data-posts-id="${post.post_id}">
                         <svg class="h-4 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"/>
                         </svg>
