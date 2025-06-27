08324f7d3830161616f0e307b62d597e2addda86
Emmanuel Obeng <emmallob14@gmail.com>
2025-06-27 10:31:13 +0000

    Increase version

--- app/Controllers/Templates.php
+++ app/Controllers/Templates.php
@@ -57,7 +57,7 @@ class Templates extends BaseController
 
         return [
             'baseUrl' => $urlPath,
-            'version' => '1.2.60',
+            'version' => '1.2.70',
             'privacyVersion' => '1.0',
             'postRadius' => $this->defaultRadius,
             'privacyUpdatedDate' => 'June 20, 2025',
